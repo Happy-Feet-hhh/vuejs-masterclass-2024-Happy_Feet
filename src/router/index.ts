4e2d330be1ad8811ef31e6d748e95834e350c076
Happy_Feet <498949493@qq.com>
2026-03-31 15:23:33 +0800

    04-改为基于文件的路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,33 +1,14 @@
-import { h } from "vue";
 import { createRouter, createWebHistory } from "vue-router";
+import { routes, handleHotUpdate } from "vue-router/auto-routes";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      // component: HomeView,
-      component: () => import("@/views/HomeView.vue"),
-    },
-    {
-      path: "/projects",
-      name: "projects",
-      component: () => import("@/views/ProjectsView.vue"),
-    },
-    {
-      // 动态路由：/projects/{id}
-      path: "/projects/:id",
-      name: "SingleProject",
-      component: () => import("@/views/SingleProjectView.vue"),
-    },
-    {
-      path: "/:catchAll(.*)*",
-      name: "NotFound",
-      // h() 可以生成一个html元素，import { h } from "vue";
-      component: h("p", { style: "color: red;" }, "404 Not Found"),
-    },
-  ],
+  routes,
 });
+
+// This will update routes at runtime without reloading the page
+if (import.meta.hot) {
+  handleHotUpdate(router);
+}
 
 export default router;
