b0f878ebe26462d4be8f914da1ac79e2d5f0955d
Happy_Feet <498949493@qq.com>
2026-03-31 12:59:29 +0800

    更改为动态加载和动态路由

---
+++ src/views/SingleProjectView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+
+const route = useRoute();   // 可以使用useRoute获取当前路径的动态信息(如path参数)
+const params = route.params;
+</script>
+
+<template>
+    <div>
+        <h1>项目 {{ params?.id }} </h1>
+    </div>
+</template>
